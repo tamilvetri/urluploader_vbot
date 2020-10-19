9ad40be071fb46783da6c1f08c9b48d1b0e82daf
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-19 16:59:32 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hi,{message.chat.first_name} {m.from_user.first_name}
+    START_TEXT = """Hi{},
 This is a Telegram URL Upload Bot!
 
 <b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
@@ -7,21 +7,27 @@ This is a Telegram URL Upload Bot!
 /help for more details.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
+    ABOUT_TEXT = """Hi __{}__,
+**📝 Language:** Python 3
+**🧰 Framework:** Pyrogram
+**👨‍💻 Developer:** [shareads_admin](t.me/shareads_admin)
+**📮 Channel:** [NS BOT UPDATES](https://t.me/v_bots)
+**👥 leech_Group:** [V_Leech](https://t.me/joinchat/OrrAak4wsBqS0ctCa3DpnQs)"""
+    
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = "<b>Lets start,📥 Downloading to my server</b>"
+    UPLOAD_START = "<b>Downloading complete , 📤 now uploading</b> "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @TGBotsZ"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**☑️ file is successfuly UPLOADED Thank you for Using Me 👇 .**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @TGBotsZ \nUploaded in {} seconds."
-    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT = "Please /about your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    SAVED_CUSTOM_THUMB_NAIL = "<b>✅ Custom thumbnail saved.THIS IS PERMANENT THUMBNAIL,Start rename ! Enjoy</b>."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "<b>✅Your thumbnail has been successfully deleted</b>."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = " "
@@ -41,15 +47,8 @@ Expires on: 31/12/2020"""
    SVideo - Give File as video with Screenshots
    DFile  - Give File with Screenshots
    Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
-   
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
+   DFile  - Give File without Screenshots """
 
---------
-Send /me to know current plan details
-
-Support Group : @InFoTelGroup
-© @TGBotsZ"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
