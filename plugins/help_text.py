650483634bc3820ae5fd9167fed74a3579c21806
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-20 11:17:52 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -67,7 +67,7 @@ async def start(bot, update):
     TRChatBase(update.from_user.id, update.text, "/start")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT,
+        text=Translation.START_TEXT.format(update.from_user.first_name, Config.USER_NAME), 
         parse_mode="html",
         disable_web_page_preview=True,
         #reply_to_message_id=update.message_id
