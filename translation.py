2f902556f5114c28186e9617286516ea229fa7ec
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-20 18:45:22 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -16,7 +16,7 @@ This is a Telegram URL Upload Bot!
     URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     DOWNLOAD_START = "<b>Lets start,📥 Downloading to my server  </b>\n {} "
-    UPLOAD_START = "<b>Downloading complete , 📤 now uploading</b> \n {} "
+    UPLOAD_START = "<b>Downloading complete , 📤 now uploading</b> \n <code>{}</code>  "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**☑️ file is successfuly UPLOADED Thank you for Using Me 👇 .**"
