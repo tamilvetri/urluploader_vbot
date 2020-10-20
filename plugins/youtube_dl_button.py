577f75d37f533c2c8e41f5170479ae68ff50bc6a
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-20 21:36:45 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -98,7 +98,7 @@ async def youtube_dl_call_back(bot, update):
                 l = entity.length
                 youtube_dl_url = youtube_dl_url[o:o + l]
     await bot.edit_message_text(
-        text=Translation.DOWNLOAD_START.format(youtube_dl_url,TG_MAX_FILE_SIZE),
+        text=Translation.DOWNLOAD_START.format(youtube_dl_url,await humanbytes(filesize)),
         chat_id=update.message.chat.id,
         message_id=update.message.message_id
     )
@@ -201,7 +201,7 @@ async def youtube_dl_call_back(bot, update):
             )
             logger.info(images)
             await bot.edit_message_text(
-                text=Translation.UPLOAD_START.format(custom_file_name , TG_MAX_FILE_SIZE),
+                text=Translation.UPLOAD_START.format(custom_file_name , await humanbytes(filesize)),
                 chat_id=update.message.chat.id,
                 message_id=update.message.message_id
             )
