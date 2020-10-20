50dd287d790f29e7cdef55caef368e2c6c63c6f8
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-20 21:00:01 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -47,7 +47,7 @@ async def progress_for_pyrogram(
             ''.join(["⌑" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "custom_file_name {0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + " {0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
