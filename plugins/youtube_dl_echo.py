51a76d5bc1434995fac5f2d4ecdc3de5ccbfdef5
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-21 12:23:15 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -234,7 +234,7 @@ async def echo(bot, update):
         )
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,custom_file_name,
+            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
@@ -259,7 +259,7 @@ async def echo(bot, update):
         reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(""),
+            text=Translation.FORMAT_SELECTION.format(""),custom_file_name,
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
