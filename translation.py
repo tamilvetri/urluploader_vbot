b83e14ba7f02a1403818084732bae647778ca239
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-10-21 11:54:47 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,7 +8,7 @@ This is a Telegram URL Upload Bot!
     
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \n<b>File Name</b> <code>{}</code>
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
     URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
