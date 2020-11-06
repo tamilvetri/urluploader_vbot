0a76570ba0f67f3cd7622843ef444be7cb07ee8c
tamilvetri <65523392+tamilvetri@users.noreply.github.com>
2020-11-06 14:57:57 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -234,7 +234,7 @@ async def echo(bot, update):
         )
         await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.DOWNLOAD_START,
+            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + .format(custom_file_name)
             reply_markup=reply_markup,
             parse_mode="html",
             reply_to_message_id=update.message_id
